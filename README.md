e40360055fd21e8592bdd54edc6090643aa81297
elwood-berry <eberry@anterasoftware.com>
2021-07-07 16:40:44 -0500

    Updated delete dialog

--- README.md
+++ README.md
@@ -43,3 +43,4 @@ See 'attr-gird.component.html'
 1. Display the charges menu  
 1. Display/Format Delete button
 1. Confirmation dialog for deleting
+1. Format: Look & Feel - Deletion Confirmation Dialog Window
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -16,6 +17,7 @@ import { AttrFooterComponent } from './components/attr-footer/attr-footer.compon
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     MaterialLibraryModule,
     FlexLayoutModule

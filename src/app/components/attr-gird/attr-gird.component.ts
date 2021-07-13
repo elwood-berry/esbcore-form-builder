95592b7bcdc1161b8649bfa49c0d3a165a613352
elwood-berry <eberry@anterasoftware.com>
2021-07-13 13:51:24 -0500

    2 files modified

--- src/app/components/attr-gird/attr-gird.component.ts
+++ src/app/components/attr-gird/attr-gird.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { DataAttributesService } from '../../services/data-attributes/data-attributes.service';
 
 @Component({
   selector: 'app-attr-gird',
@@ -9,21 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 //
 export class AttrGirdComponent implements OnInit {
-  // PROPERTIES
-  public demoUrl = 'https://jsonplaceholder.typicode.com/posts';
-
-  // CONSTRUCTUR
-  constructor() {
-    // json.getData('../../assets/data/attributes.json').subscribe(result => {
-    //   console.log(result);
-    // });
-    // this.http
-    //   .get(this.demoUrl)
-    //   .toPromise()
-    //   .then(data => {
-    //     console.log('DATA', data);
-    //   });
-  }
+  constructor() {}
 
   ngOnInit() {}
 }

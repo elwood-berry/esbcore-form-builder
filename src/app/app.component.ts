c5ad6c2720fba68c47402b90115fe561fe061bb2
elwood-berry <eberry@anterasoftware.com>
2021-07-14 10:43:43 -0500

    4 files modified, 2 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'attr-grid',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
 
